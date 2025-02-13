0deaba6b24c3564ecab6ac2f8a43d7579973192d
MuhammedYahiya <yahiyahmhd@gmail.com>
2025-02-13 19:21:27 +0530

    error fixed

--- model/prediction.py
+++ model/prediction.py
@@ -18,9 +18,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
-# Make predictions on test data
-y_pred = knn.predict(X_test)
-
 # Save the model
 filename = 'knn.pkl'
 pickle.dump(knn, open(filename,'wb'))
@@ -33,21 +30,23 @@ try:
     print("Model parameters:", model.get_params())
 
     # New data for prediction
-    new_data = np.array([[52,1,1,134,201,0,1,158,0,0.8,2,1,2]])
-    print("New data:", new_data)
+    new_data = np.array([[47,1,2,108,243,0,1,152,0,0,2,0,2]])
 
-    # Reshape new_data array
-    new_data = new_data.reshape(1, -1)
+    # Convert to DataFrame with column names
+    feature_names = X.columns  # Get feature names from training data
+    new_data_df = pd.DataFrame(new_data, columns=feature_names)
 
     # Make prediction on new data
-    prediction = model.predict(new_data)
+    prediction = model.predict(new_data_df)
     print("Prediction:", prediction)
+
+    # Get the predicted class probabilities
+    class_probabilities = model.predict_proba(new_data_df)
+    print("Class probabilities:", class_probabilities)
+
+    # Calculate the percentage based on the probabilities
+    percentage = class_probabilities[0][1] * 100
+    print("Percentage prediction:", percentage)
+
 except Exception as e:
     print("Error loading the model:", e)
-# Get the predicted class probabilities
-class_probabilities = model.predict_proba(new_data)
-print("Class probabilities:", class_probabilities)
-
-# Calculate the percentage based on the probabilities
-percentage = class_probabilities[0][1] * 100
-print("Percentage prediction:", percentage)
